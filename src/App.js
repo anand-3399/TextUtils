8bebb37b4800918fc93c526875c4c2e459462fee
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-12-22 20:06:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import './App.css';
+import Alert from './components/Alert';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+import React from "react";
+
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
+
+
+function App() {
+
+  const [mode, setMode] = useState('light');  // Wheather dark mode is enabled or not
+
+  // Now making a state named as Alert
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    // This will show the alert
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    // This will hide the alert after 2 seconds
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  // Now this will set the Alert to an OBJECT
+
+  const togglemode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#0d0d3f';
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+
+      /*
+      // Setting the title of the page to change after every 2 seconds
+      setInterval(() => {
+        document.title = "TextUtils is Amazing";
+      }, 2000);
+
+      setInterval(() => {
+        document.title = "Install TextUtils Now";
+      }, 1500);
+      */
+
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
+    }
+  }
+
+
+  return (
+    <>
+      <Router>
+
+        <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} togglemode={togglemode} />
+        {/* <Navbar /> */}
+        <Alert alert={alert} />
+
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about"
+              element={<About />} />
+            {/* 
+              /users => Users component
+              /users/home => User home component
+              but react will do partial matching hence we use exact
+            */}
+            <Route exact path="/"
+              element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+      </Router>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const [text, setText] = useState('')
+    // text = "new text" //wrong way to update state
+    // setText("new text") //correct way to update state
+
+    const handleUpClick = (e) => {
+        // console.log("Upper case was clicked");
+        let newText = text.toUpperCase();
+        // setText("you clicked on handleUpClick")
+        setText(newText)
+        props.showAlert("Converted to UpperCase", "success");
+    }
+
+    const handleLoClick = (e) => {
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to LowerCase", "success");
+    }
+
+    const handleClearClick = (e) => {
+        let newText = '';
+        setText(newText)
+        props.showAlert("Text has been cleared", "success");
+    }
+
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value)
+    }
+
+    const handleCopy = () => {
+        var text = document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text has been copied", "success");
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces have been removed", "success");
+    }
+
+    return (
+        <>
+            <div className='container' style={{color: props.mode === 'dark'?'white':'#0d0d3f' }} >
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{backgroundColor: props.mode === 'dark'?'grey':'white' , color: props.mode === 'dark'?'white':'#0d0d3f'}} ></textarea>
+                </div>
+
+                {/* Functions of buttons */}
+                <button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className='btn btn-primary mx-1' onClick={handleLoClick}>Convert to LowerCase</button>
+                <button className='btn btn-primary mx-1' onClick={handleClearClick}>Clear Text</button>
+                <button className='btn btn-primary mx-1' onClick={handleCopy}>Copy Text</button>
+                <button className='btn btn-primary mx-1' onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            </div>
+
+
+            <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#0d0d3f' }} >
+                <p>{text.split(" ").length} words and {text.length} characters </p>
+                <p>{0.008 * text.split(" ").length} Minutes Read </p>
+                <h2>Preview</h2>
+                <p>{text.length>0?text:"Enter something in the text box above to preview it here" }</p>
+            </div>
+        </>
+    )
+}
